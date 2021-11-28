5c49b520c24b1c9b27cbbbcb94e0dd358331c13b
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2021-11-29 00:07:05 +0330

    Set Updated

--- Apriori/Set.cs
+++ Apriori/Set.cs
@@ -9,6 +9,13 @@ namespace Apriori
     class Set<T> where T : IItem
     {
         private List<T> ElementsList;
+        int Count
+        {
+            get
+            {
+                return ElementsList.Count;
+            }
+        }
         public T this[int i]
         {
             get { return ElementsList.ElementAt(i); }
@@ -18,7 +25,7 @@ namespace Apriori
             get
             {
                 string str = "{";
-                for(int i = 0; i < ElementsList.Count; i++)
+                for (int i = 0; i < ElementsList.Count; i++)
                 {
                     str += ElementsList.ElementAt(i).InString;
                     if (i < ElementsList.Count - 1)
@@ -33,24 +40,53 @@ namespace Apriori
         {
             ElementsList = new List<T>();
         }
-        public Set(T Item) : this()
+        public Set(T Element) : this()
         {
-            ElementsList.Add(Item);
+            ElementsList.Add(Element);
         }
-        public Set(IEnumerable<T> Items) : this()
+        public Set(IEnumerable<T> Elements) : this()
         {
-            foreach (T Item in Items)
+            foreach (T Element in Elements)
             {
-                if (!ElementsList.Contains(Item))
-                    ElementsList.Add(Item);
+                if (!ElementsList.Contains(Element))
+                    ElementsList.Add(Element);
             }
         }
-        public void AddItem(T Item)
+        public void AddElement(T Element)
         {
-            if (!ElementsList.Contains(Item))
-                ElementsList.Add(Item);
+            if (!ElementsList.Contains(Element))
+                ElementsList.Add(Element);
         }
+        public bool Contains(T Element)
+        {
+            return ElementsList.Contains(Element);
+        }
+        public bool HaveSubSet(Set<T> set)
+        {
+            if (set == null)
+                return true;
+            for (int i = 0; i < set.Count; i++)
+                if (!Contains(set[i]))
+                {
+                    return false;
+                }
+            return true;
+        }
+        public bool IsSubSetOf(Set<T> set)
+        {
+            if (set == null)
+                if (Count == 0)
+                    return true;
+                else
+                    return false;
 
+            for (int i = 0; i < Count; i++)
+                if (!set.Contains(this[i]))
+                {
+                    return false;
+                }
+            return true;
+        }
         public void Print()
         {
             Console.WriteLine(InString);
