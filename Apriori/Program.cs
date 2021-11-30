3374e7676f5dbb35dfe73614940a2da7e4cb2d6a
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2021-11-30 19:01:35 +0330

    SubSet_Test added to Program class

--- Apriori/Program.cs
+++ Apriori/Program.cs
@@ -10,6 +10,22 @@ namespace Apriori
     {
         static void Main(string[] args)
         {
+            
+        }
+        static void SubSet_Test()
+        {
+            Set<MyItem> set = new Set<MyItem>();
+            set.Add(new MyItem("i1"));
+            set.Add(new MyItem("i2"));
+            set.Add(new MyItem("i3"));
+            set.Add(new MyItem("i4"));
+
+            List<Set<MyItem>> Subsets = new List<Set<MyItem>>(set.SubSets());
+            foreach (Set<MyItem> subset in Subsets)
+                subset.Print();
+            Console.WriteLine(Subsets.Last().IsEqual(set));
+            Console.WriteLine("*");
+            Console.ReadLine();
         }
     }
 }
