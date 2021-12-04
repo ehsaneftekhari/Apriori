24437ac09a14cebf72300ccd5ec6c7e4b24d9f58
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2021-12-04 14:04:15 +0330

    MyApriori_Test()

--- Apriori/Program.cs
+++ Apriori/Program.cs
@@ -10,7 +10,36 @@ namespace Apriori
     {
         static void Main(string[] args)
         {
-            
+            MyApriori_Test();
+        }
+        static void MyApriori_Test()
+        {
+            MyItem i1 = new MyItem("i1");
+            MyItem i2 = new MyItem("i2");
+            MyItem i3 = new MyItem("i3");
+            MyItem i4 = new MyItem("i4");
+            MyItem i5 = new MyItem("i5");
+            MyItem i6 = new MyItem("i6");
+            MyItem i7 = new MyItem("i7");
+            MyItem i8 = new MyItem("i8");
+            MyItem i9 = new MyItem("i91");
+            MyItem i10 = new MyItem("i10");
+            MyItem i11 = new MyItem("i11");
+            Transaction<MyItem> T1 = new Transaction<MyItem>(new MyItem[] { i1, i2, i3, i4, i5, i6 });
+            Transaction<MyItem> T2 = new Transaction<MyItem>(new MyItem[] { i2, i3, i4, i5, i6, i7 });
+            Transaction<MyItem> T3 = new Transaction<MyItem>(new MyItem[] { i1, i4, i5, i8 });
+            Transaction<MyItem> T4 = new Transaction<MyItem>(new MyItem[] { i1, i4, i6, i9, i10 });
+            Transaction<MyItem> T5 = new Transaction<MyItem>(new MyItem[] { i2, i4, i5, i10, i11 });
+
+            MyApriori<MyItem> Ap = new MyApriori<MyItem>();
+            Ap.MinSupport = 0.6;
+            Ap.MinConfident = 0.6;
+            Ap.AddTransaction(T1);
+            Ap.AddTransaction(T2);
+            Ap.AddTransaction(T3);
+            Ap.AddTransaction(T4);
+            Ap.AddTransaction(T5);
+            Ap.Process();
         }
         static void SubSet_Test()
         {
